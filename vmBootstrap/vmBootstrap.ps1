df1bd1e86764468b8fd3a59585f304251d836614
Andrea Maruccia <andrea.maruccia@bluecieloecm.com>
2017-05-17 13:20:40 +0200

    added very basic dsc (to be extended)

---
+++ vmBootstrap/vmBootstrap.ps1
@@ -0,0 +1,14 @@
+Configuration vmBootstrap
+{
+  param ($someparam)
+
+  Node localhost
+  {
+    Environment EnvironmentExample
+    {
+        Ensure = "Present"
+        Name = "TestEnvironmentVariable"
+        Value = "TestValue"
+    }   
+  }
+} 
