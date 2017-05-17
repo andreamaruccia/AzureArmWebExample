0a107be9d044f52f88bafdb0cc72b3012aed04e8
Andrea Maruccia <andrea.maruccia@bluecieloecm.com>
2017-05-17 17:12:28 +0200

    Install choco and nginx

--- vmBootstrap/vmBootstrap.ps1
+++ vmBootstrap/vmBootstrap.ps1
@@ -1,14 +1,33 @@
 Configuration vmBootstrap
 {
-  param ($someparam)
-
+  Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+  Import-DscResource -Module cChoco  
+  
   Node localhost
   {
-    Environment EnvironmentExample
-    {
-        Ensure = "Present"
-        Name = "TestEnvironmentVariable"
-        Value = "TestValue"
-    }   
+      LocalConfigurationManager
+      {
+          DebugMode = 'ForceModuleImport'
+      }
+      cChocoInstaller installChoco
+      {
+        InstallDir = "c:\choco"
+      }
+      cChocoPackageInstaller installNginx
+      {
+        Name        = "nginx-service"
+        DependsOn   = "[cChocoInstaller]installChoco"
+        Version     = "1.6.2.1"
+      }
+      Service NginxService
+      {
+          Name        = "nginx"
+          StartupType = "Automatic"
+          State       = "Running"
+          DependsOn   = "[cChocoPackageInstaller]installNginx"
+      } 
   }
 } 
+
+vmBootstrap
+Start-DscConfiguration .\vmBootstrap -wait -Verbose -force
