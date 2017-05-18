344b6cf21784476297f7577ceb213aa4b69ac2df
Andrea Maruccia <andrea.maruccia@bluecieloecm.com>
2017-05-18 16:49:26 +0200

    disable firewall in order to serve webtraffic

--- vmBootstrap/vmBootstrap.ps1
+++ vmBootstrap/vmBootstrap.ps1
@@ -1,6 +1,6 @@
 Configuration vmBootstrap
 {
-  Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+  Import-DscResource -ModuleName PSDesiredStateConfiguration
   Import-DscResource -Module cChoco
   
   Node localhost
@@ -26,5 +26,25 @@ Configuration vmBootstrap
           State       = "Running"
           DependsOn   = "[cChocoPackageInstaller]installNginx"
       } 
+      Script DisableFirewall 
+      {
+          GetScript = {
+              @{
+                  GetScript = $GetScript
+                  SetScript = $SetScript
+                  TestScript = $TestScript
+                  Result = -not('True' -in (Get-NetFirewallProfile -All).Enabled)
+              }
+          }
+  
+          SetScript = {
+              Set-NetFirewallProfile -All -Enabled False -Verbose
+          }
+  
+          TestScript = {
+              $Status = -not('True' -in (Get-NetFirewallProfile -All).Enabled)
+              $Status -eq $True
+          }
+      }
   }
 }
